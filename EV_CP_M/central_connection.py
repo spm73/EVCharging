a08a4a45847f8ccabc682f1d406b12d81e599560
spm73 <sergiopascual05@gmail.com>
2025-10-19 20:26:25 +0200

    Sockets almost there

--- EV_CP_M/central_connection.py
+++ EV_CP_M/central_connection.py
@@ -1,34 +1,33 @@
 import socket
 
 from central_not_responding_exception import CentralNotRespondingException
+from cp_status import CPStatus
 
 class CentralConnection:
     WAITING_SUPPLY_MSG = 'wait-supply'.encode()
     
-    def __init__(self, ip_addr, port_number, status_msg):
+    def __init__(self, ip_addr, port_number):
         self.connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.connection.connect((ip_addr, port_number))
-        self.msg = status_msg
         
-    def authorize(self, engine_id) -> bool:
-        self.connection.sendall(engine_id.encode())
+    def authorize(self, cp_id) -> bool:
+        self.connection.sendall(cp_id.encode())
         answer = self.connection.recv(3).decode()
         if not answer:
             raise CentralNotRespondingException()
         return answer != "BAD"
         
-    def send_status_message(self):
-        current_status = self.msg.encode()
-        self.connection.sendall(current_status)
+    def send_status_message(self, current_status: CPStatus):
+        self.connection.sendall(current_status.value)
         answer = self.connection.recv(3).decode()
         if not answer:    
             raise CentralNotRespondingException()
       
-    def waiting_supply(self): 
-        self.connection.sendall(CentralConnection.WAITING_SUPPLY_MSG)
-        answer = self.connection.recv(2).decode()
-        if not answer:
-            raise CentralNotRespondingException()
+    # def waiting_supply(self): 
+    #     self.connection.sendall(CentralConnection.WAITING_SUPPLY_MSG)
+    #     answer = self.connection.recv(2).decode()
+    #     if not answer:
+    #         raise CentralNotRespondingException()
         
         
     # def run(self, engine_id):
