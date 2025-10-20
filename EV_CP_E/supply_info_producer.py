db449a360d7611bc70564546f4179250ad42e1aa
spm73 <sergiopascual05@gmail.com>
2025-10-20 23:10:57 +0200

    Implemented kafka classes for data shared during supply

--- EV_CP_E/supply_info_producer.py
+++ EV_CP_E/supply_info_producer.py
@@ -1,4 +1,33 @@
+from json import dumps
+
 from confluent_kafka import Producer
 
 class SupplyInfoProducer:
-    pass
+    TOPIC_NAME = 'supply-data'
+    def __init__(self, kafka_ip: str, kafka_port: int):
+        conf = {
+            'bootrsap.servers': f'{kafka_ip}:{kafka_port}'
+        }
+        self.producer = Producer(conf)
+        
+    def send_supplying_msg(self, consumption: float, price: float, supply_id: int):
+        msg = {
+            'supply_id': supply_id,
+            'type': 'supplying',
+            'consumption': consumption,
+            'price': price
+        }
+        
+        json_msg = dumps(msg)
+        self.producer.produce(SupplyInfoProducer.TOPIC_NAME, value=json_msg)
+    
+    def send_ticket(self, total_consumption: float, total_price: float, supply_id: int):
+        msg = {
+            'supply_id': supply_id,
+            'type': 'ticket',
+            'consumption': total_consumption,
+            'price': total_price
+        }
+        
+        json_msg = dumps(msg)
+        self.producer.produce(SupplyInfoProducer.TOPIC_NAME, value=json_msg)
--- EV_Driver/supply_info_consumer.py
+++ EV_Driver/supply_info_consumer.py
@@ -1,4 +1,35 @@
+from json import loads
+from typing import Any
+
 from confluent_kafka import Consumer
 
 class SupplyInfoConsumer:
-    pass
+    SUBSCRIBED_TOPIC = ['supply-data']
+    def __init__(self, kafka_ip: str, kafka_port: int):
+        conf = {
+            'bootstrap.servers': f'{kafka_ip}:{kafka_port}',
+            'group.id': 'central',
+            'auto.offset.reset': 'latest', # process everything not committed
+            'enable.auto.commit': True, # manual commit management
+        }
+        self.consumer = Consumer(conf)
+        self.consumer.subscribe(SupplyInfoConsumer.SUBSCRIBED_TOPIC)
+        
+    def close(self):
+        self.consumer.close()
+        
+    def get_request(self, supply_id: int) -> dict[str: Any] | None:
+        try:
+            raw_msg = self.consumer.poll(timeout=1.0)
+            if not raw_msg:
+                return None
+            
+            if raw_msg.error():
+                pass
+            
+            msg = loads(raw_msg.value().decode('utf-8'))
+            if msg['supply_id'] != supply_id:
+                return None
+            return msg
+        except RuntimeError:
+            print("Consumer is closed and cannot make operations")
