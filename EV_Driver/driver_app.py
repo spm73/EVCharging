893b0afd0f52c6f124432cd0a0e242207d5f895b
niso1-ua <152491134+niso1-ua@users.noreply.github.com>
2025-10-25 15:01:59 +0200

    Add files via upload

--- EV_Driver/driver_app.py
+++ EV_Driver/driver_app.py
@@ -1,4 +1,5 @@
 import time
+import sys
 
 if __name__ == "__main__":
     CPs = []
@@ -21,12 +22,52 @@ if __name__ == "__main__":
         if cp in CPs:
             print(f"Connecting with CP {cp}...")
             #enviar una peticion a central a cp
+
+            running = True 
+            while running:
+                time_plug = 0 #leer de kafka
+                #Suponemos que son puntos semirápidos(AC) de 22kw
+                amount = 0 #leer de kafka
+                cost = 0 #leer de kafka
+                #enviar los datos donde sea
+                print(f"\r{' ' * 60}\rTime = {time_plug:.2f}s    Amount = {amount:.4f}kwh   Cost = {cost:.4f}€", end="")
+                sys.stdout.flush()
+
+                #cada vez debe leer de kafka para ver si hay ticket
+                if time_plug == 0:
+                    #si lo hay para el bucle
+                    running = False
+
+            #imprimir ticket
+
             next_cp = (CPs.index(cp) + 1) % len(CPs)
+
         elif cp == "quit":
             break
         else:
             print(f"Connecting with CP {CPs[next_cp]}...")
             #enviar una peticion a central a CPs[next_cp]
+
+
+            running = True 
+            while running:
+                time_plug = 0 #leer de kafka
+                #Suponemos que son puntos semirápidos(AC) de 22kw
+                amount = 0 #leer de kafka
+                cost = 0 #leer de kafka
+                #enviar los datos donde sea
+                print(f"\r{' ' * 60}\rTime = {time_plug:.2f}s    Amount = {amount:.4f}kwh   Cost = {cost:.4f}€", end="")
+                sys.stdout.flush()
+
+                #cada vez debe leer de kafka para ver si hay ticket
+                if time_plug == 0:
+                    #si lo hay para el bucle
+                    print("\n")
+                    running = False
+
+            #imprimir ticket
+
+
             next_cp = (next_cp + 1) % len(CPs)
         time.sleep(4)
             
