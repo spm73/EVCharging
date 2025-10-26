0907f8bcab11d7612faa2c58df3dd115061c0bab
spm73 <sergiopascual05@gmail.com>
2025-10-26 21:15:17 +0100

    Error topic between central and driver, forgot upload files

---
+++ EV_Central/supply_error_producer.py
@@ -0,0 +1,19 @@
+from json import dumps
+
+from confluent_kafka import Producer
+
+class SupplyResProducer:
+    TOPIC_NAME = 'supply-error'
+    def __init__(self, kafka_ip: str, kafka_port: int):
+        conf = {
+            'bootstrap.servers': f'{kafka_ip}:{kafka_port}'
+        }
+        self.producer = Producer(conf)
+        
+    def send_error(self, supply_id: int):
+        msg_content = {
+            'supply_id': supply_id,
+            'msg': 'Engine has closed down abruptly, we are sorry'
+        }
+        msg = dumps(msg_content)
+        self.producer.produce(SupplyResProducer.TOPIC_NAME, value=msg)
---
+++ EV_Driver/supply_error_consumer.py
@@ -0,0 +1,39 @@
+from json import loads
+from typing import Any
+
+from confluent_kafka import Consumer, KafkaError, KafkaException
+
+class SupplyResConsumer:
+    SUBSCRIBED_TOPIC = ['supply-error']
+    def __init__(self, kafka_ip: str, kafka_port: int, driver_id: str, supply_id: int):
+        conf = {
+            'bootstrap.servers': f'{kafka_ip}:{kafka_port}',
+            'group.id': driver_id,
+            # following configs can be ommited
+            'auto.offset.reset': 'latest',
+            'enable.auto.commit': True,
+        }
+        self.consumer = Consumer(conf)
+        self.consumer.subscribe(SupplyResConsumer.SUBSCRIBED_TOPIC)
+        self.supply_id = supply_id
+        
+    def close(self):
+        self.consumer.close()
+        
+    def get_error(self) -> dict[str, Any] | None:
+        try:
+            raw_msg = self.consumer.poll(timeout=1.0)
+            if not raw_msg:
+                return None
+            
+            if raw_msg.error():
+                error_code = raw_msg.error().code()
+                if error_code != KafkaError._PARTITION_EOF:
+                    raise KafkaException(raw_msg.error())
+            
+            msg = loads(raw_msg.value().decode('utf-8'))
+            if msg['supply_id'] != self.supply_id:
+                return None
+            return msg
+        except RuntimeError:
+            print("Consumer is closed and cannot make operations")
