9f96c72c3eb0ebd52c3ea437e4acdbb7fef8e70b
spm73 <sergiopascual05@gmail.com>
2026-04-27 01:56:02 +0200

    Modification in producer
    Only 1 partition for all

--- communications/src/communications/kafka/KafkaProducer.py
+++ communications/src/communications/kafka/KafkaProducer.py
@@ -14,10 +14,9 @@ class KafkaProducer:
     def get_topic(self) -> str:
         return self.__topic
     
-    def send_message(self, message: Message, key: str) -> None:
+    def send_message(self, message: Message) -> None:
         self.__producer.produce(
             self.__topic,
             value=message.to_payload(),
-            key=key
         )
         self.__producer.flush()
