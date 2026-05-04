97800ab6e0c376eac84a89c2ad049ffc6b41c622
spm73 <sergiopascual05@gmail.com>
2026-05-04 02:22:59 +0200

    Implemented message handler

---
+++ communications/src/communications/sockets/MessageHandler.py
@@ -0,0 +1,19 @@
+from typing import Callable
+
+class MessageHandler:
+    def __init__(self, default: Callable[[str], str] | None =None):
+        self.__handlers: dict[str, Callable[[str], str]] = {}
+        self.__default = default
+
+    def register(self, message_type, callback) -> None:
+        self.__handlers[message_type] = callback
+        
+    def delete(self, message_type) -> None:
+        del self.__handlers[message_type]
+
+    def handle(self, message) -> str | None:
+        message_type = message.split('#')[0]
+        handler = self.__handlers.get(message_type, self.__default)
+        if handler is None:
+            return None
+        return handler(message)
