c03a492e3cf0e648b42e5579a645c71b572734c1
spm73 <sergiopascual05@gmail.com>
2026-05-19 19:26:27 +0200

    Implemented main for registry

--- EV_Registry/src/EV_Registry/main.py
+++ EV_Registry/src/EV_Registry/main.py
@@ -1,5 +1,20 @@
+from fastapi import FastAPI
+from uvicorn import run
+from os import getenv
+
+from routes import router
+
 def main():
-    print("Hello from ev-registry!")
+    app = FastAPI()
+    app.include_router(router)
+    
+    run(
+        "main:app",
+        host="0.0.0.0",
+        port=int(getenv('API_PORT')),
+        ssl_keyfile='/certs/server.key',
+        ssl_certfile='/certs/server.crt'
+    )
 
 
 if __name__ == "__main__":
