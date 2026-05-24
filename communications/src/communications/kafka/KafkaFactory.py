57e334b98a98f9682691c2e38731261562551c3e
spm73 <sergiopascual05@gmail.com>
2026-05-24 22:46:34 +0200

    Changes in kafka factory

--- communications/src/communications/kafka/KafkaFactory.py
+++ communications/src/communications/kafka/KafkaFactory.py
@@ -1,23 +1,21 @@
-from abc import ABC, abstractmethod
-from typing import Type
+from typing import Type, Callable
 
 from .KafkaBrokerInfo import KafkaBrokerInfo
 from .KafkaProducer import KafkaProducer
 from .KafkaConsumer import KafkaConsumer, M
-from .Message import Message
 
-class AbstractKafkaFactory(ABC):
+class KafkaFactory:
     def __init__(self, broker_info: KafkaBrokerInfo) -> None:
         self._broker_info = broker_info
         
     def create_producer(self, topic: str) -> KafkaProducer:
         return KafkaProducer(self._broker_info, topic)
     
-    @abstractmethod
     def create_consumer(
         self, 
         topic: str, 
         group_id: str, 
-        message_class: Type[M]
+        message_class: Type[M],
+        filter_func: Callable[[M], bool] | None = None
         ) -> KafkaConsumer[M]:
-        pass
+        return KafkaConsumer(self._broker_info, topic, group_id, message_class, filter_func)
