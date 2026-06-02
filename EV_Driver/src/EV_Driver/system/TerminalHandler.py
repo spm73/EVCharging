6e093b1378a178c93bef8d61543615c63603dbef
Nico <nicolassirvent2005@gmail.com>
2026-06-02 15:05:31 +0200

    un commit muy grande lo siento

---
+++ EV_Driver/src/EV_Driver/system/TerminalHandler.py
@@ -0,0 +1,54 @@
+import os
+import queue
+from threading import Thread, Event
+from .events import *
+
+
+class TerminalHandler:
+    # Ahora recibe la cola central desde fuera
+    def __init__(self, cola_central: queue.Queue):
+        self.__cola_central = cola_central 
+        self.__is_listening = Event()
+        self.__thread = Thread(target=self.__listening_loop, daemon=True)
+        self.__thread.start()
+
+    def start_listening(self) -> None:
+        self.__is_listening.set()
+
+    def stop_listening(self) -> None:
+        self.__is_listening.clear()
+
+    def __listening_loop(self):
+        self.__is_listening.wait()
+        
+        while self.__is_listening.is_set():
+            try:
+                comando = input()
+                if self.__is_listening.is_set():
+                    # Empaquetamos el input en el formato estándar y lo enviamos
+                    evento = SystemEvent(intention=Intention.KEYBOARD_INPUT, data=comando)
+                    self.__cola_central.put(evento)
+            except EOFError:
+                break
+    
+# --- MÉTODOS ESTÁTICOS ---
+
+    @staticmethod
+    def clean():
+        """Limpia la consola independientemente del sistema operativo."""
+        # 'nt' es Windows, el resto (Linux/Mac) usan 'clear'
+        os.system('cls' if os.name == 'nt' else 'clear')
+
+
+    @staticmethod
+    def printCP(cp_list: list) -> None:
+        TerminalHandler.clean();
+        if not cp_list: 
+            print("loading Charging Points from central...\nThis may take a few seconds")
+            return
+        
+        print("Available Charging Points")
+        for indice, elemento in enumerate(cp_list, start=1):
+            print(f">>>>> [{indice}] {elemento}")
+
+        print("<<<< select one Charging point >>>>")
